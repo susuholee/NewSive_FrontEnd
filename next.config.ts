2e45be0f023cf20f15134d657c157cadbf24e0f0
blockSuho <akakak695@naver.com>
2026-02-02 20:45:05 +0900

    chore: 빌드 파일 환경설정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images : {
-    domains : ['localhost'],
-  }
-
+  output: "export",         
+  images: {
+    unoptimized: true,     
+  },
 };
 
 export default nextConfig;
